78b458517a25258d0a227d3ea09f6e9675936ad8
Thomas Donovan <tdonovan79@gmail.com>
2019-11-11 00:23:28 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,21 @@
-# Your Code Here
+#base map function
+def map (array)
+  new = []
+  index = 0
+  while index < array.length do
+   new.push(yield(array[index]))
+    index += 1
+  end
+  return new
+end
+
+#base reduce function
+def reduce (array, start = 0)
+  index = 0
+  total = start
+  while index < array.length do
+    total = yield(total, array[index])
+    index += 1
+  end
+  return total == 0 ? true : total
+end
